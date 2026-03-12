18f0b46b9c7867e68c9e5ca31a4243cff2fed27d
Shaik0Ayman <146252387+Shaik0Ayman@users.noreply.github.com>
2026-03-12 19:09:18 +0530

    fin

--- ananya/firewall/apply_firewall.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-iptables-restore < firewall/firewall.rules
--- ananya/monitor/traffic_monitor.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Monitoring traffic..."
-
-while true
-do
-    ip -s link show wlan0
-    sleep 5
-done
